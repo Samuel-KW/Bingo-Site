06ab4500f0618b9e0c5cb1e7218ed4ecd432b8c9
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2024-07-31 16:14:46 -1000

    fix: grouped related code

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,30 +1,74 @@
-import { Router, Request, Response } from "express";
+import express, { Router, Request, Response, NextFunction } from "express";
+import expressSession from "express-session";
+import cookieParser from "cookie-parser";
 
-import { getAllBoards, getAllUsers, db } from "Database";
+import { getAllBoards, getAllUsers, getUserByUUID, db } from "Database";
 
 import LogIn from "./api/Login";
 import SignUp from "./api/Signup";
 import LogOut from "./api/Logout";
+
 import { GetOwnedBoards } from "./api/GetOwnedBoards";
 import { GetParticipatingBoards } from "./api/GetParticipatingBoards";
 
 import { CreateBoard } from "../routes/api/CreateBoard";
 import { BoardLookup } from "../routes/api/BoardLookup";
-import { doubleCsrfProtection, generateToken } from "../src/Authentication";
+
+import { User } from "../src/User";
+import { AuthenticatedRequest, SessionRequest } from "../src/Server";
+import { doubleCsrfProtection, generateToken, sessionOptions } from "../src/Authentication";
+
+declare module "express-session" {
+  interface SessionData {
+    user: {
+			id: string;
+		};
+  }
+
+	interface Locals {
+		user: User;
+		csrfToken: string;
+	}
+}
 
 // Initialize the router
 const router = Router();
 
+// Initialize request handlers
+router.use(cookieParser(sessionOptions.secret));
+router.use(expressSession(sessionOptions));
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
+console.log("\tInitialized Express middleware.");
+
 // Initialize CSRF protection
 router.use(doubleCsrfProtection);
-console.log("\tCSRF protection initialized.");
+console.log("\tDouble CSRF authentication initialized.");
 
 // Generate CSRF tokens
 router.get("/api/csrf", (req: Request, res) => {
 	const csrf = generateToken(req, res);
 	res.json({ csrf });
 });
-console.log("\tDouble CSRF authentication initialized.");
+
+// Initialize session authentication, add user object to request
+router.use(function (req: Request | SessionRequest, res: Response, next: NextFunction) {
+
+	const uuid = req.session.user?.id;
+
+	// If the user is authenticated, set the user object
+	if (uuid) {
+		const user = getUserByUUID(uuid);
+		if (user)
+			(req as AuthenticatedRequest).user = user;
+	}
+
+	// Generate CSRF token
+	res.locals["csrfToken"] = generateToken(req, res);
+
+	next();
+});
+console.log("\tSession authentication initialized.");
 
 // TODO REMOVE THEM LATER
 router.get("/api/sessions", (_req: Request, res: Response) => {
@@ -48,8 +92,9 @@ router.post("/api/createBoard", CreateBoard);
 
 router.post("/api/login", LogIn);
 router.post("/api/signup", SignUp);
-router.post("/api/logout", LogOut);
 
-console.log("\tInitialized authentication routes.");
+router.get("/api/logout", LogOut);
+
+console.log("\tInitialized API routes.");
 
 export default router;
--- backend/routes/pages.ts
+++ backend/routes/pages.ts
@@ -1,56 +1,15 @@
-import express, { Request, Response, NextFunction, Router } from "express";
-import expressSession from "express-session";
-import cookieParser from "cookie-parser";
+import express, { Router, Request, Response } from "express";
 import path from "path";
-
-import { generateToken, sessionOptions } from "../src/Authentication";
-import { getUserByUUID } from "../Database";
-import { User } from "../src/User";
-import { AuthenticatedRequest, SessionRequest } from "src/Server";
-
-declare module "express-session" {
-  interface SessionData {
-    user: {
-			id: string;
-		};
-  }
-
-	interface Locals {
-		user: User;
-		csrfToken: string;
-	}
-}
 
 const router = Router();
 
 // Serve the React app
 router.use(express.static(path.resolve("../build")));
-console.log("\tServing React application.");
 
-// Initialize request handlers
-router.use(cookieParser(sessionOptions.secret));
-router.use(expressSession(sessionOptions));
-router.use(express.urlencoded({ extended: false }));
-router.use(express.json());
-console.log("\tInitialized Express middleware.");
-
-// Initialize session authentication, add user object to request
-router.use(function (req: Request | SessionRequest, res: Response, next: NextFunction) {
-
-	const uuid = req.session.user?.id;
-
-	// If the user is authenticated, set the user object
-	if (uuid) {
-		const user = getUserByUUID(uuid);
-		if (user)
-			(req as AuthenticatedRequest).user = user;
-	}
-
-	// Generate CSRF token
-	res.locals["csrfToken"] = generateToken(req, res);
-
-	next();
+router.get(/^\/(account|play)/i, (_req: Request, res: Response) => {
+	res.sendFile(path.resolve("../build/index.html"));
 });
-console.log("\tSession authentication initialized.");
+
+console.log("\tServing React application.");
 
 export default router;
